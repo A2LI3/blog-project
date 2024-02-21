eaf7cde1598ffa645ce08a8eb7f1e48fd0fb953e
A2LI3 <155317747+A2LI3@users.noreply.github.com>
2024-02-21 07:32:58 +0000

    created mainlayout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-        <p className='text-amber-500'>
-          Hello
-        </p>
+          <HomePage/>
     </div>
   );
 }
