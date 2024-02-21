c5b0c10aa2f0ac19505328fdc919a260f9eccfcd
A2LI3 <155317747+A2LI3@users.noreply.github.com>
2024-02-21 07:09:30 +0000

    Creates Project Structrue

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p className='text-amber-500'>
+          Hello
+        </p>
+    </div>
+  );
+}
+
+export default App;
