6e7937cc337380823cf2680b665ee3b14a0c6d39
A2LI3 <155317747+A2LI3@users.noreply.github.com>
2024-03-03 08:47:55 +0000

    3 - Creating Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
 import { images } from '../constants'
 
+const navItemsInfo = [
+  {name:"Home"},
+  {name:"Articles"},
+  {name:"Pages"},
+  {name:"Pricing"},
+  {name:"Faq"},
+  
+]
+
+const NavItem = ({name}) => {
+  return (
+  <li className="relative group">
+  <a href="/" className="px-4 py-2">
+    {name}
+    </a>
+<span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-100 group-hover:opacity-100">
+  /
+  </span>
+</li>
+);
+}
+
 const Header = () => {
   return <section>
     <header className="container mx-auto px-5 flex justify-between py-4 items-center">
@@ -9,22 +31,9 @@ const Header = () => {
     </div>
       <div className="flex gap-x-9 items-center">
         <ul className="flex gap-x-5 font-semibold">
-          <li className="bg-red-500 relative group">
-            <a href="/" className="px-4 py-2">Home</a>
-          <span className="text-blue-500 absolute transition-all duration-500 font-bold></span>
-          </li>
-          <li>
-          <a href="/">Articles</a>
-          </li>
-          <li>
-            <a href="/">Pages</a>
-          </li>
-          <li>
-            <a href="/">Pricing</a>
-          </li>
-          <li>
-            <a href="/">Faq</a>
-          </li>
+          {navItemsInfo.map((item) => (
+            <NavItem Key ={item.name} name={item.name}/>
+          ))}
         </ul>
         <button className="border-2 border-blue-500 px-6 py-3 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">Sign in</button>
       </div>
